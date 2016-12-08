d8db3ced6743659f9b92b1cec2b10733750b18fc
ckhuram7 <ckhuram7@users.noreply.github.com>
2016-12-08 00:18:37 -0500

    Init Commit

---
+++ template.js
@@ -0,0 +1,594 @@
+var sketchProc=function(processingInstance){ with (processingInstance){
+size(400, 400); 
+frameRate(60);
+
+
+//ProgramCodeGoesHere
+
+// Khuram Chughtai
+// Final Project
+angleMode = "radians";
+
+// Global Variables Defined
+var start = 0;
+var gameover = 0;
+var restart = 0;
+var moving = null;
+var c = [{x:100,y:210},{x:100,y:260},{x:100,y:310}];
+var mycontrol; 
+var clear = 1;
+var lives = 5;
+var level = 0;
+var score = 0;
+var time = 20;
+var milli = 0;
+var nextlevel = 0;
+// End of Global Variables
+
+
+/////////////////////////////////////////
+////////////Animal Objects///////////////
+/////////////////////////////////////////
+var animalObj = function(x, y,dir) {
+    this.x = x;
+    this.y = y;
+    this.currFrame = frameCount;
+    this.i = 0;
+    this.dir = dir;
+};
+
+var animal1 = new animalObj(110, 110);
+
+animalObj.prototype.draw = function() {
+    //this.x = mouseX;
+    //this.y = mouseY;
+    switch (this.i) {
+        case 0:
+            fill(166, 149, 149);
+            ellipse(this.x, this.y, 15, 25);  // Body
+            fill(0, 0, 0);
+            ellipse(this.x-3,this.y+7,2,2);  // Eye L
+            ellipse(this.x+3,this.y+7,2,2);  // Eye R
+            ellipse(this.x,this.y-12,2,8);   // Tail
+            ellipse(this.x,this.y-16,2,6);  // Tail extension
+            fill(166, 149, 149);
+            ellipse(this.x-8,this.y+5,8,8);  // Ear L
+            ellipse(this.x+8,this.y+5,8,8);  // Ear R
+            fill(0, 0, 0);
+            line(this.x-3, this.y+10, this.x-8, this.y+12);  // Whisker 1 L
+            line(this.x+3, this.y+10, this.x+8, this.y+12);  // Whisker 1 R
+            line(this.x-3, this.y+12, this.x-10, this.y+14); // Whisker 2 L
+            line(this.x+3, this.y+12, this.x+10, this.y+14); // Whisker 2 R
+            break;
+        case 1:
+            fill(166, 149, 149);
+            ellipse(this.x, this.y, 15, 25);  // Body
+            fill(0, 0, 0);
+            ellipse(this.x-3,this.y+7,2,2);  // Eye L
+            ellipse(this.x+3,this.y+7,2,2);  // Eye R
+            ellipse(this.x,this.y-12,2,8);   // Tail
+            ellipse(this.x,this.y-16,3,6);  // Tail extension
+            fill(166, 149, 149);
+            ellipse(this.x-8,this.y+5,8,8);  // Ear L
+            ellipse(this.x+8,this.y+5,8,8);  // Ear R
+            fill(0, 0, 0);
+            line(this.x-3, this.y+10, this.x-5, this.y+12);  // Whisker 1 L
+            line(this.x+3, this.y+10, this.x+5, this.y+12);  // Whisker 1 R
+            line(this.x-3, this.y+12, this.x-8, this.y+14); // Whisker 2 L
+            line(this.x+3, this.y+12, this.x+8, this.y+14); // Whisker 2 R
+            break;
+        case 2:
+            fill(166, 149, 149);
+            ellipse(this.x, this.y, 15, 25);  // Body
+            fill(0, 0, 0);
+            ellipse(this.x-3,this.y+7,2,2);  // Eye L
+            ellipse(this.x+3,this.y+7,2,2);  // Eye R
+            ellipse(this.x,this.y-12,2,8);   // Tail
+            ellipse(this.x,this.y-16,4,6);  // Tail extension
+            fill(166, 149, 149);
+            ellipse(this.x-8,this.y+5,8,8);  // Ear L
+            ellipse(this.x+8,this.y+5,8,8);  // Ear R
+            fill(0, 0, 0);
+            line(this.x-3, this.y+10, this.x-8, this.y+12);  // Whisker 1 L
+            line(this.x+3, this.y+10, this.x+8, this.y+12);  // Whisker 1 R
+            line(this.x-3, this.y+12, this.x-10, this.y+14); // Whisker 2 L
+            line(this.x+3, this.y+12, this.x+10, this.y+14); // Whisker 2 R
+            break;
+    }
+    if (this.currFrame < (frameCount - 30)) {
+        this.currFrame = frameCount;
+        this.i++;
+        if (this.i > 2) {
+            this.i = 0;
+        }
+    }
+
+};
+
+animalObj.prototype.move =function(){
+    if(this.dir === 1) {
+        this.x += 1;
+        if ((this.x > 390) || (this.x < 10)) {
+            this.x = 12;
+        }
+    }
+    else if (this.dir === 0) {
+        this.x -= 1;
+        if ((this.x > 390) || (this.x < 10)) {
+            this.x = 390;
+        }  
+    }
+    // Collision Check with walls
+    //for (var i=0; i<walls.length; i++) {
+    //    if (dist(this.x, this.y, walls[i].x-10, walls[i].y-25) < 20) {
+    //        this.x = this.x;
+    //        this.y = this.y;
+    //    }
+    //}
+};
+
+
+/////////////////////////////////////////
+////////////Space Ship Obj///////////////
+/////////////////////////////////////////
+// Defination of SpaceShipObject
+var spaceShipObj = function(x,y,dir,r,g,b){
+    this.r = r;  this.g = g;  this.b = b;  // THis allows for different colors
+    this.dir = dir;  // This allows for the movement of different directions
+    this.x = x;      //  The X and Y cordinates 
+    this.y = y;
+};
+// SpaceShipObj that I Control
+//var myControl = new spaceShipObj(350,350,1,82,87,82);
+// Space Ship Prototype Draw Function
+spaceShipObj.prototype.display = function(){
+    fill (this.r, this.g, this.b);   // Changes the Color of the Objects created
+    ellipse(this.x,this.y,12,8); // Top Circle 
+    ellipse(this.x,this.y+8,20,10);  // Makes the Shape of the Body 
+    fill (0, 217, 255); // Changes the color to blue for the Windows
+    rect(this.x-8 ,this.y+6,3,3); // Window 1
+    rect(this.x -3,this.y+6,3,3); // Window 2
+    rect(this.x + 2,this.y+6,3,3); // Window 3
+    fill(138, 28, 138);  // Fills the Color of the Tires to Black.
+    ellipse(this.x-10,this.y+10,4,4); // Most Left Bottom Tire
+    ellipse(this.x-6,this.y+13,4,4); // Second One
+    ellipse(this.x-2,this.y+13,4,4); // Third One
+    ellipse(this.x+2,this.y+13,4,4); // Forth One
+    ellipse(this.x+6,this.y+13,4,4); // Fifth One
+    ellipse(this.x+10,this.y+10,4,4); // Most Right Bottom Tire
+    fill(71, 18, 82);
+    ellipse(this.x,this.y+2,12,2); // Top Bar
+};
+// Space Ship Move Function
+spaceShipObj.prototype.move =function(){
+    if(this.dir === 1) {
+        this.x += 1;
+        if ((this.x > 390) || (this.x < 10)) {
+            this.x = 12;
+        }
+    }
+    else if (this.dir === 0) {
+        this.x -= 1;
+        if ((this.x > 390) || (this.x < 10)) {
+            this.x = 390;
+        }  
+    }
+    //if (dist(this.x, this.y, mycontrol.x, mycontrol.y) < 14) {
+    //    gameover = 1;
+    //}
+};
+//spaceShipObj.prototype.checkCollision = function() {
+//    if (dist(this.x, this.y, myControl.x, myControl.y) < 14) {
+//        gameover = 1;
+//    }
+//};
+
+/////////////////////////////////////////
+////////////Turkey Obj  /////////////////
+/////////////////////////////////////////
+
+var tankObj = function(x, y) {
+    this.position = new PVector(x, y);
+    //this.state = [new haltState(), new turnState(), new chaseState()];
+    this.currState = 0;
+    this.angle = 0;
+    this.whisker1 = new PVector(0, 0);
+    this.whisker2 = new PVector(0, 0);
+};
+
+tankObj.prototype.draw = function() {
+    //noFill();
+    fill (219, 44, 164);
+    ellipse(this.position.x-7,this.position.y-4,5,10);
+    fill (237, 176, 23);
+    ellipse(this.position.x-3,this.position.y-4,6,10);
+    fill (72, 191, 53);
+    ellipse(this.position.x+3,this.position.y-4,6,10);
+    fill (61, 142, 179);
+    ellipse(this.position.x+7,this.position.y-4,5,10);
+    fill (92, 52, 52);
+    ellipse(this.position.x,this.position.y,16,12);
+    fill (43, 12, 43);
+    ellipse(this.position.x-2,this.position.y+3,5,5);
+    ellipse(this.position.x+2,this.position.y+3,5,5);
+       // for (var i = 0; i < points.length; i++) {
+        //    vertex(points[i].x, points[i].y);   
+        ///vertex(points[0].x, points[0].y);
+    //endShape();
+     
+};
+
+
+/////////////////////////////////////////
+////////////Tile Maps  /////////////////
+/////////////////////////////////////////
+
+// These will be used in later portions of the project.
+var tileMap = [
+   "wwwwwwwwwwwwwwwwwwww",
+    "w          ww      w",
+    "w wwwww          www",
+    "w w  w     ww   wwww",
+    "w      www  w      w",
+    "w w w            www",
+    "w      www   ww    w",
+    "w   ww          wwww",
+    "w  ww       www   ww",
+    "w    www        wwww",
+    "w      w    ww w   w",
+    "w  ww            www",
+    "w  w         ww wwww",
+    "w      www   ww    w",
+    "w ww            wwww",
+    "w      wwwww       w",
+    "w      wwwww    wwww",
+    "w                ww",
+    "w     ww    ww  wwww",
+    "wwwwwwwwwwwwwwwwwwww"];
+    
+    
+var tilemap2 = [
+        "                    ",
+        "                    ",
+        "                    ",
+        "      w             ",
+        "       w            ",
+        "        w           ",
+        "         w          ",
+        "       c  w         ",
+        "           w        ",
+        "            w       ",
+        "             w      ",
+        "       w            ",
+        "      w             ",
+        "     w              ",
+        "    w               ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "      w             ",
+        "       w            ",
+        "        w           ",
+        "         w          ",
+        "      c   w         ",
+        "           w        ",
+        "            w       ",
+        "             w      ",
+        "       w            ",
+        "      w             ",
+        "     w              ",
+        "    w               ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "      w             ",
+        "       w            ",
+        "        w           ",
+        "         w          ",
+        "      c   w         ",
+        "           w        ",
+        "            w       ",
+        "             w      ",
+        "       w            ",
+        "      w             ",
+        "     w              ",
+        "    w               ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "      w             ",
+        "       w            ",
+        "        w           ",
+        "         w          ",
+        "      c   w         ",
+        "           w        ",
+        "            w       ",
+        "             w      ",
+        "       w            ",
+        "      w             ",
+        "     w              ",
+        "    w               ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "      w             ",
+        "       w            ",
+        "        w           ",
+        "         w          ",
+        "      c   w         ",
+        "           w        ",
+        "            w       ",
+        "             w      ",
+        "       w            ",
+        "      w             ",
+        "     w              ",
+        "    w               ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "            w       ",
+        "                    ",
+        "    w               ",
+        "                    ",
+        "                    ",
+        "                    ",];
+/////////////////////////////////////////
+////////////Loading Dock/////////////////
+/////////////////////////////////////////
+// Object for the space ship to land on.
+var loadingDock = function(x,y,dir){
+   this.x = x;
+   this.y = y;
+   this.dir = dir;
+};
+
+// Display Function to create the Object
+loadingDock.prototype.display = function(){
+    fill(74, 37, 27);
+    rect(this.x,this.y,25,5);
+};
+// Loading Dock Move Function to allow for both backwards and Front Moving Functions
+loadingDock.prototype.move =function(){
+    if(this.dir === 1) {
+        this.x += 1;
+        if ((this.x > 390) || (this.x < 10)) {
+            this.x = 12;
+        }
+    }
+    else if (this.dir === 0) {
+        this.x -= 1;
+        if ((this.x > 390) || (this.x < 10)) {
+            this.x = 390;
+        }  
+    }
+    //if (dist(this.x, this.y, myControl.x, myControl.y) < 18) {
+    //    myControl.x = this.x;
+    //}
+    //if(myControl.y < 170)
+    //{
+    //    if (dist(this.x, this.y, myControl.x, myControl.y) > 35) {
+    //        gameover = 1;
+    //    }    
+    //}
+};
+
+
+/////////////////////////////////////////
+////////////Player Controls//////////////
+/////////////////////////////////////////
+// Function to Start the Game
+var mouseDragged = function () {
+    for (var i = 0; i < c.length; i ++) {
+        if (dist (mouseX, mouseY, c[i].x, c[i].y) < 10) {
+            moving = i;
+        }
+    }
+};
+
+var mouseMoved = function () {
+    moving = null;
+};        
+var mouseClicked = function() {
+    //start = 1;
+};
+// Player Movements Using the Arrow Keys
+var keyPressed = function() {
+    if (keyCode === ENTER) {
+       start++;
+       clear = 0;
+    }
+    // Player Movements Using the Arrow Keys
+    if (keyCode === LEFT) {
+        mycontrol.x-= 6;
+    }
+    if (keyCode === RIGHT) {
+        mycontrol.x+= 6;
+    }
+    if (keyCode === UP) {
+        mycontrol.y-= 4;
+    }
+    if (keyCode === DOWN) {
+        mycontrol.y+= 4;
+    }
+};
+/////////////////////////////////////////
+///Animals for start screen Animation ///
+/////////////////////////////////////////
+var animals = [new animalObj(30,30,1),new animalObj(70,70,1),new animalObj(90,90,0),new animalObj(120,130,1),new animalObj(150,150,0),new animalObj(90,170,1)];
+var spaceShips = [new spaceShipObj(150,300,1,100,200,82),new spaceShipObj(10,270,0,200,200,82),new spaceShipObj(150,200,0,0,200,82),new spaceShipObj(10,230,0,200,200,82)];
+
+var spaceShipsLevelOne = [new spaceShipObj(150,100,1,100,200,82),new spaceShipObj(10,210,0,200,200,82),new spaceShipObj(150,290,0,0,200,82),new spaceShipObj(10,230,0,200,200,82)];
+
+var docks = [new loadingDock(30,30,0),new loadingDock(70,70,1),new loadingDock(90,90,0),new loadingDock(120,130,0),new loadingDock(150,150,1),new loadingDock(90,170,0)];
+
+/////////////////////////////////////////
+////////////Main Draw Function///////////
+/////////////////////////////////////////
+var draw = function() {
+        if (start === 0 ){
+        background(220,243,245); // Sets Background to Blue
+        fill(255, 0, 255);
+        textSize(40);
+        if(level === 0)
+        {
+            if(start === 0 && clear === 1)
+            {
+                for (var i = 0; i< animals.length; i++){
+                    animals[i].draw();
+                    animals[i].move();
+                    //animals[i].display();
+                }
+                for (var i = 0; i< spaceShips.length; i++){
+                    spaceShips[i].move();
+                    spaceShips[i].display();
+                }
+            }
+        }
+        text("Animal Attack", 100, 180);
+        text("Click enter to begin", 30, 350);
+        text("Khuram Chughtai", 45, 380);
+        restart = 0;
+    }
+    else if( start === 1 && gameover === 0){
+        clear = 0;
+        textSize(16);
+        background(220,243,245); // Sets Background to Blue
+        text("Welcome this game has many levels that you will be able to enjoy and collect             perks from as you go through.  You will start off Dodging several different                 obstatcles as you lead up to get the ability to shoot your obstacles and move on             to different parts.  The robots will start to get more advanced as you move on,             you will get points for beating the clocks and killing obstacles as you progress             .",50,50,300,300);
+    }
+    else if (start === 2 && gameover === 0){
+        textSize(25);
+        var r = round ((c[0].x - 100) / 200 * 255);
+        var g = round ((c[1].x - 100) / 200 * 255);
+        var b = round ((c[2].x - 100) / 200 * 255);
+        background(220,243,245);
+        fill(0, 0, 0);
+        text("R", 70, 220);
+        rect(100, 200, 200, 20, 29);
+        text("G", 70, 270);
+        rect(100, 250, 200, 20, 29);
+        text("B", 70, 320);
+        rect(100, 300, 200, 20, 29);
+        fill(158, 158, 158);
+        noStroke();
+        
+        for (var i = 0; i < c.length; i ++) {
+            line (c[i].x, 100, c[i].x, 300);
+            fill (255, 255, 255);
+            ellipse (c[i].x, c[i].y, 20, 20);
+        }
+        
+        if (moving !== null) {
+            c[moving].x = constrain (mouseX, 100,300);
+        }
+        //animal1.draw();
+        //animal1.move();
+        //rect(xPos, yPos, 17, 40, 20);
+        var drawspaceship = new spaceShipObj(190,100,1,r,g,b);
+        drawspaceship.display();
+        mycontrol = drawspaceship;
+        level = 1;
+        mycontrol.x = 350;
+        mycontrol.y = 370;
+        //var sample = spaceShipObj(190,100,1,r,g,b);
+        //sample.display();
+        text("Click enter to begin", 90, 350);
+    }
+    else if (start === 3 && gameover === 0 && nextlevel === 0)
+    {
+        background(220,243,245);
+        textSize(16);
+        milli++;
+        if(milli ===100){
+             time--;
+             milli=0;
+        }
+        if(time < 1)
+        {
+            gameover = 1;   
+        }
+        text("Round One: Dodge",0,20);
+        text("Level: " + level,0,35);
+        text("Lives: " + lives,69,35);
+        text("Time: " + time ,330,20);
+        text("Score: " + score ,330,35);
+        mycontrol.display();
+        if(level === 1 )
+        {
+             for (var i = 0; i< spaceShipsLevelOne.length; i++){
+                spaceShipsLevelOne[i].move();
+                spaceShipsLevelOne[i].display();  
+                if (dist(spaceShipsLevelOne[i].x, spaceShipsLevelOne[i].y, mycontrol.x,                     mycontrol.y) < 30) {
+                    lives--;
+                    mycontrol.x = 350;
+                    mycontrol.y = 360;
+                }
+            }
+        }
+        if(mycontrol.y < 10)
+        {
+            level = 2;
+            start = 3;
+            nextlevel = 1;
+        }
+    }
+    else if (start === 3 && gameover === 0 && nextlevel === 1)
+    {
+        background(220,243,245);
+        score = score + time;
+        text("Congrats click enter to go to next level",150,150,100,100); 
+        mycontrol.y = 380;
+        mycontrol.x = 100;
+        
+    }
+    else if (start === 4 && gameover === 0 && nextlevel === 1)
+    {
+        background(220,243,245);
+        textSize(16);
+        milli++;
+        if(milli ===100){
+             time--;
+             milli=0;
+        }
+        if(time < 1)
+        {
+            gameover = 1;   
+        }
+        text("Round One: Dodge",0,20);
+        text("Level: " + level,0,35);
+        text("Lives: " + lives,69,35);
+        text("Time: " + time ,330,20);
+        text("Score: " + score ,330,35);
+        mycontrol.display();
+        if(level === 2 )
+        {
+             for (var i = 0; i< spaceShipsLevelOne.length; i++){
+                spaceShipsLevelOne[i].move();
+                spaceShipsLevelOne[i].display();  
+                if (dist(spaceShipsLevelOne[i].x, spaceShipsLevelOne[i].y, mycontrol.x,                     mycontrol.y) < 30) {
+                    lives--;
+                    mycontrol.x = 350;
+                    mycontrol.y = 360;
+                }
+            }
+        }
+        
+    }
+    else if (gameover === 1){
+        background(102, 255, 0);
+        fill(255, 0, 255);
+        textSize(20);
+        text("Try again", 100, 80);
+        textSize(48);
+        text("Game Over", 100, 150);
+        restart = 1;
+    }
+};
+
+
+}};
